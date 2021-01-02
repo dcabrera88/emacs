3334d967cd19c3ee61d48d394f11845e0ff8a330
david.cabrera.88@gmail.com <dcabrera88@DESKTOP-A823I2O.localdomain>
2021-01-02 10:22:49 -0600

    Adding refin, refout, time functions for cards.

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -11,9 +11,34 @@
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c b") 'org-switchb)
 
 ;; Simple default note file
+(setq org-directory "~/Org/")
 (setq org-default-notes-file (concat org-directory "/inbox.org"))
+(setq org-agenda-files (directory-files-recursively org-directory ".org$"))
+
+(defun org-add-refin ()
+  (interactive)
+  (let (refin)
+    (setq refin (read-string "Enter value to append to REFIN property: "))
+    (org-entry-add-to-multivalued-property (point) "REFIN" refin)))
+
+(defun org-add-refout ()
+  (interactive)
+  (let (refout)
+    (setq refout (read-string "Enter value to append to REFOUT property: "))
+    (org-entry-add-to-multivalued-property (point) "REFOUT" refout)))
+
+(defun org-add-date-update ()
+  (interactive)
+  (let (temptime)
+    (with-temp-buffer
+      (setq temptime (replace-regexp-in-string "[a-zA-Z ]+" "" (org-time-stamp-inactive)))
+      )
+    (org-entry-add-to-multivalued-property (point) "MODIFIED-DATE" temptime)
+    )
+  )
 
 ;; Add any org agenda file as a possible refile target
 (setq org-refile-targets '((nil :maxlevel . 9)
